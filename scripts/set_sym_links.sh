318725e9809d69321ea7a8e751449690460b2de8
eduardodgdebem <bemeduardode@gmail.com>
2025-08-09 14:06:20 -0300

    Fixing script

--- scripts/set_sym_links.sh
+++ scripts/set_sym_links.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-# Use $HOME instead of ~
 if [ ! -f "$HOME/.zshrc" ]; then
+  ln -s "$PWD/zsh/.zshrc" "$HOME/.zshrc"
+else
+  rm "$HOME/.zshrc"
   ln -s "$PWD/zsh/.zshrc" "$HOME/.zshrc"
 fi
 
-if [ ! -d "$HOME/Pictures/wallpapers" ]; then # Use -d for directories
+if [ ! -d "$HOME/Pictures/wallpapers" ]; then
+  ln -s "$PWD/wallpapers" "$HOME/Pictures/wallpapers"
+else
+  rm -rf "$HOME/Pictures/wallpapers"
   ln -s "$PWD/wallpapers" "$HOME/Pictures/wallpapers"
 fi
 
-if [ ! -d "$HOME/.config/ghostty" ]; then # Use -d for directories
+if [ ! -d "$HOME/.config/ghostty" ]; then
   ln -s "$PWD/ghostty" "$HOME/.config/ghostty"
-fi
-
-if [ ! -f "$HOME/.config/ghostty/config" ]; then # Use -d for directories
-  ln -s "$PWD/ghostty/config" "$HOME/.config/ghostty/config"
+else
+  rm -rf "$HOME/.config/ghostty"
+  ln -s "$PWD/ghostty" "$HOME/.config/ghostty"
 fi
