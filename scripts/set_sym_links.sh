4b7bf2e3c55a046543a1c84c2fb7aea09660e513
eduardodgdebem <bemeduardode@gmail.com>
2025-08-09 13:57:43 -0300

    Adding wallpapers

--- scripts/set_sym_links.sh
+++ scripts/set_sym_links.sh
@@ -8,3 +8,11 @@ fi
 if [ ! -d "$HOME/Pictures/wallpapers" ]; then # Use -d for directories
   ln -s "$PWD/wallpapers" "$HOME/Pictures/wallpapers"
 fi
+
+if [ ! -d "$HOME/.config/ghostty" ]; then # Use -d for directories
+  ln -s "$PWD/ghostty" "$HOME/.config/ghostty"
+fi
+
+if [ ! -f "$HOME/.config/ghostty/config" ]; then # Use -d for directories
+  ln -s "$PWD/ghostty/config" "$HOME/.config/ghostty/config"
+fi
